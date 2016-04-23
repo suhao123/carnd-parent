b9964220dfbdad0ea533b249dd7cb5c06ada7507
hao.su <hao.su@haosudeMacBook-Pro.local>
2016-04-23 14:29:37 +0800

    init

---
+++ src/main/java/com/canrd/patent/App.java
@@ -0,0 +1,7 @@
+package com.canrd.patent;
+
+/**
+ * Created by hao.su on 16/4/13.
+ */
+public class App {
+}
---
+++ src/main/java/com/canrd/patent/server/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.canrd.patent.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by hao.su on 16/4/13.
+ */
+@Controller
+public class LoginController {
+
+    @RequestMapping("/")
+    public Object index() {
+        return "index";
+    }
+}
